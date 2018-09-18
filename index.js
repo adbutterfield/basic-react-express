d223ea45f3df4b66b8f246e5e5269789ac59efb4
Adam Butterfield <adb707@yahoo.com>
2018-09-18 11:59:26 +0900

    add basic error handling

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
-const logSomething = (options) => ({
-  ...options,
-  anotherOption: 'Hello!'
+import express from 'express';
+import path from 'path';
+
+// Express app setup
+const app = express();
+const port = 3000;
+
+// view engine
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'pug');
+
+// routes
+app.get('*', (req, res) => {
+  res.render('index');
 });
 
-const options = logSomething({ one: '1', two: '2' });
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-console.log(options);
+// error handler
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {},
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
